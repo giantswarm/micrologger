b2b74cbe82038fa3e8ec44efb6ade551f25927c4
Theo Brigitte <theo.brigitte@gmail.com>
2022-02-11 13:16:34 +0100

    log to stderr

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Change DefaultIOWriter from Stdout to Stderr.
+
 ## [0.6.0] - 2021-12-14
 
 ### Changed
--- default.go
+++ default.go
@@ -9,7 +9,7 @@ import (
 
 var DefaultCaller = newCallerFunc(0)
 
-var DefaultIOWriter = os.Stdout
+var DefaultIOWriter = os.Stderr
 
 var DefaultTimestampFormatter kitlog.Valuer = func() interface{} {
 	return time.Now().UTC().Format("2006-01-02T15:04:05.999999-07:00")
